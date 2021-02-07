40b8ecfc3ef995916482a4e572fabcac87c1f9b0
PauloASC <pauloantoniosampaio@gmail.com>
2021-02-07 16:25:09 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# What this application will do.
+
+# Open all the Excel files
+# For each file:
+# Verify if there's any value in that file is greater than 55.000
+#in the Sales colunm
+# If the number is greater than 55.000 a text message will be
+# sent to the Name associated to that sale. In the text will be
+# included their Name, the month of the sale and how much he/she
+# sold
